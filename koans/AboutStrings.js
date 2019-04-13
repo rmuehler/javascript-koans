9025a9807e0bc9e72b13a36c63745c7f86679f19
Protosac <protosacq@gmail.com>
2019-04-13 10:25:16 -0400

    Add more solutions.

--- koans/AboutStrings.js
+++ koans/AboutStrings.js
@@ -1,21 +1,22 @@
 describe("About Strings", function() {
 
   //We shall contemplate truth by testing reality, via spec expectations.
-  it("should manipulate strings", function() {
-    var emptyString = '';
-    expect(typeof(emptyString)).toBe(FILL_ME_IN); //A mistake? - http://javascript.crockford.com/remedial.html
-    expect(emptyString.length).toBe(FILL_ME_IN);
+  it("should use strings", function() {
+      var emptyString = '';
+      expect(typeof(emptyString)).toBe('string'); //A mistake? - http://javascript.crockford.com/remedial.html
+      expect(emptyString.length).toBe(0);
+  });
 
+  it("should slice strings", function() {
     var greeting = "hello, world!";
-    expect(greeting.slice(0,1)).toBe(FILL_ME_IN);
-    expect(greeting.slice(1,3)).toBe(FILL_ME_IN);
-    expect(greeting.slice(2,-1)).toBe(FILL_ME_IN);
-    expect(greeting.toUpperCase()).toBe(FILL_ME_IN);
+    expect(greeting.slice(0,1)).toBe('h');
+    expect(greeting.slice(1,3)).toBe('el');
+    expect(greeting.slice(2,-1)).toBe('llo, world');
+    expect(greeting.toUpperCase()).toBe('HELLO, WORLD!');
 
     var hello = "hello";
     var world = "world";
-    expect(hello + world).toBe(FILL_ME_IN);
-
+    expect(hello + world).toBe('helloworld');
   });
 
   it("should understand string literals", function () {
@@ -23,7 +24,7 @@ describe("About Strings", function() {
     var personalGreeting = `How is your day, ${name}?`;
     var wrongGreeting = "How is your day, ${name}?";
 
-    expect(personalGreeting).toBe(FILL_ME_IN);
-    expect(wrongGreeting).toBe(FILL_ME_IN);
+    expect(personalGreeting).toBe('How is your day, Jane?');
+    expect(wrongGreeting).toBe('How is your day, ${name}?');
   });
 });
---
+++ koans/TODAY.md
@@ -0,0 +1,7 @@
+# Questions
+
+- The learning roller coaster
+- Ginger.io
+- Encourage more slack questions and DMs
+- No competitions
+- I'll post more code snippets
